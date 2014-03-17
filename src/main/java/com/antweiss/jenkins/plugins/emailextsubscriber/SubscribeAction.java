8e130c77e80c9b685bc30cab4d2ba4b452fcfa69
David Greenberg <dgreenberg@interwise.com>
2014-03-17 13:08:57 +0200

    added new subscribe icon

--- src/main/java/com/antweiss/jenkins/plugins/emailextsubscriber/SubscribeAction.java
+++ src/main/java/com/antweiss/jenkins/plugins/emailextsubscriber/SubscribeAction.java
@@ -16,21 +16,20 @@ public class SubscribeAction implements Action {
 
     private final AbstractProject<?, ?> project;
  
-  public SubscribeAction(final AbstractProject project) {
-    this.project = project;
+	public SubscribeAction(final AbstractProject project) {
+		this.project = project;    
+	}
     
-  }
-    
-  /**
-* Method necessary to get the side-panel included in the Jelly file
-* @return this {@link AbstractProject}
-*/
-  public AbstractProject<?, ?> getProject() {
-    return this.project;
-  }
+	/**
+	* Method necessary to get the side-panel included in the Jelly file
+	* @return this {@link AbstractProject}
+	*/
+	public AbstractProject<?, ?> getProject() {
+		return this.project;
+	}
    
-  public String getIconFileName() {
-        return "/plugin/email-ext/images/template-debugger.png";
+	public String getIconFileName() {
+        return "/plugin/email-ext-subscriber/stock_mail_send.png";
     }
 
     public String getDisplayName() {
